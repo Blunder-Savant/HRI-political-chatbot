8f4cbcc2b7ca44497b93af60626d6ff0841ed665
Your Name <you@example.com>
2020-11-01 16:26:15 -0800

    Complete homework 3

--- scripts/chatbot_node.py
+++ scripts/chatbot_node.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 import rospy
-from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 from dialogflow_ros.msg import DialogflowResult
 from dialogflow_ros.msg import DialogflowParameter
 from dialogflow_ros.msg import DialogflowContext
-
-def callback(data):
-    return 
 
 def listener():
     rospy.init_node('chatbot_node',anonymous=True)
