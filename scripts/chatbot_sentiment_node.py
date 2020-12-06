19264da2c35890169610bab2487c87ff5de9164e
Kyle Kutchek <kkutchek@oakland.edu>
2020-12-05 20:10:01 -0800

    Cleaned up code and added run-time calculation

--- scripts/chatbot_sentiment_node.py
+++ scripts/chatbot_sentiment_node.py
@@ -1,71 +1,80 @@
 #!/usr/bin/env python
-from __future__ import print_function
-from flask import jsonify
 import rospy
 import csv
-import numpy as np
-import dialogflow_v2 as dialogflow
-import roslibpy
-from std_msgs.msg import Float64, String
+import dialogflow
+from std_msgs.msg import Float64
 from dialogflow_ros.msg import DialogflowResult
-from dialogflow_ros.msg import DialogflowParameter
-from dialogflow_ros.msg import DialogflowContext
 
 session_client = dialogflow.SessionsClient()
 
-session_path = session_client.session_path("hri-project-tpsj", "vibes")
+session_path = session_client.session_path("hri-project-tpsj", "leanings")
 
 language_code = "en-US"
 
-#client = roslibpy.Ros(host='localhost', port=9090)
-#client.run()
-
 
 class listener:
-  def __init__(self):
-    rospy.Subscriber("/dialogflow_client/results", DialogflowResult, self.callback, queue_size=10)
+    def __init__(self):
+        rospy.Subscriber("/dialogflow_client/results", DialogflowResult, self.callback, queue_size=50)
+
+        self.leaning = rospy.Publisher('leaning', Float64, queue_size=50)
+        self.leaning_strength = rospy.Publisher('leaning_strength', Float64, queue_size=50)
+
+        rospy.init_node('listener', anonymous=True)
+
+        self.social_leaning = 0.0
+        self.economic_leaning = 0.0
+        self.prev_intent = ""
 
 
-    self.vibe_out = rospy.Publisher('vibe', Float64, queue_size=10)
-    self.vibe_out_strength = rospy.Publisher('vibe_strength', Float64, queue_size=10)
-    self.horizontal_alignment = rospy.Publisher('horizontal_alignment', Float64, queue_size=10)
-    self.vertical_alignment = rospy.Publisher('vertical_alignment', Float64, queue_size=10)
+    def callback(self, data):
 
+        text_input = dialogflow.types.TextInput(text=data.query_text, language_code=language_code)
 
+        query_input = dialogflow.types.QueryInput(text=text_input)
 
-    rospy.init_node('listener', anonymous=True)
-    #self.df_talker = roslibpy.Topic(client, '/vibe', 'std_msgs/Float64')
+        # Enable sentiment analysis
+        sentiment_config = dialogflow.types.SentimentAnalysisRequestConfig(
+            analyze_query_text_sentiment=True)
 
-  def callback(self, data):
-    text_input = dialogflow.types.TextInput(text=data.query_text, language_code=language_code)
+        # Set the query parameters with sentiment analysis
+        query_params = dialogflow.types.QueryParameters(
+            sentiment_analysis_request_config=sentiment_config)
 
-    query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(
+            session=session_path, query_input=query_input,
+            query_params=query_params)
 
-    # Enable sentiment analysis
-    sentiment_config = dialogflow.types.SentimentAnalysisRequestConfig(
-        analyze_query_text_sentiment=True)
-
-    # Set the query parameters with sentiment analysis
-    query_params = dialogflow.types.QueryParameters(
-        sentiment_analysis_request_config=sentiment_config)
-
-    response = session_client.detect_intent(
-        session=session_path, query_input=query_input,
-        query_params=query_params)
-
-    try:   
-        question_num = int(data.intent[1:3])  # extract question number from intent string
+        try:   
+            question_num = int(data.intent[1:3])  # extract question number from intent string
+        except ValueError:
+            question_num = 0  # not a valid intent
 
         if question_num > 1:  # test questions with relevant sentiment scores
-            csvwriter.writerow([question_num - 1, response.query_result.sentiment_analysis_result.query_text_sentiment.score])
+            sentiment = round(response.query_result.sentiment_analysis_result.query_text_sentiment.score, 4)
+            csvwriter.writerow([question_num - 1, sentiment])
             file.flush()
-            
-    except ValueError:
-        pass  # not a valid intent
 
-    self.vibe_out.publish(response.query_result.sentiment_analysis_result.query_text_sentiment.score)
-    self.vibe_out_strength.publish(response.query_result.sentiment_analysis_result.query_text_sentiment.magnitude)
+            # Calculate political leanings
+            if "+X" in self.prev_intent:
+                self.economic_leaning += (1.0 * sentiment)
+            if "-X" in self.prev_intent:
+                self.economic_leaning += (-1.0 * sentiment)
+            if "+Y" in self.prev_intent:
+                self.social_leaning += (1.0 * sentiment)
+            if "-Y" in self.prev_intent:
+                self.social_leaning += (-1.0 * sentiment)
+    
+            # Record final calcualtions if last question
+            if question_num == 21: 
+                csvwriter.writerow("")
+                csvwriter.writerow(["Final Result", "Economic leaning", "Social leaning"])
+                csvwriter.writerow(["", self.economic_leaning, self.social_leaning])
+                file.flush()
 
+        self.prev_intent = data.intent
+     
+        self.leaning.publish(response.query_result.sentiment_analysis_result.query_text_sentiment.score)
+        self.leaning_strength.publish(response.query_result.sentiment_analysis_result.query_text_sentiment.magnitude)
 
 
 if __name__ == '__main__':
@@ -76,5 +85,4 @@ if __name__ == '__main__':
             listener()
             rospy.spin()
         except rospy.ROSInterruptException:
-            #client.terminate()
             pass
